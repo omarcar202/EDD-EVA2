741b65700cfd871c1ced89c756716da4e15169ca
omarcar202 <124702322+omarcar202@users.noreply.github.com>
2023-03-17 09:11:09 -0600

    Add files via upload

---
+++ EVA2_1_UML/src/eva2_1_uml/EVA2_1_UML.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_1_uml;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_1_UML {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
