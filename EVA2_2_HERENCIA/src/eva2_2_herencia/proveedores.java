45328396b47a93c1b6d4a7b62494de808b22bdca
omarcar202 <124702322+omarcar202@users.noreply.github.com>
2023-03-17 09:12:41 -0600

    Add files via upload

---
+++ EVA2_2_HERENCIA/src/eva2_2_herencia/proveedores.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_2_herencia;
+
+/**
+ *
+ * @author invitado
+ */
+public class proveedores {
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String razonSocial;
+    private String rfc;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
+    }
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+    
+}
