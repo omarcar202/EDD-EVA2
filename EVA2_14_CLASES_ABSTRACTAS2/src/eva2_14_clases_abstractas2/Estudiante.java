b5f7c8fa525eb501b0bf4820343ae51c1c35cb6a
omarcar202 <124702322+omarcar202@users.noreply.github.com>
2023-03-17 09:52:28 -0600

    Add files via upload

---
+++ EVA2_14_CLASES_ABSTRACTAS2/src/eva2_14_clases_abstractas2/Estudiante.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_14_clases_abstractas2;
+
+/**
+ *
+ * @author invitado
+ */
+public class Estudiante extends Persona {
+
+    @Override
+    public void imprimirDatos() {
+        
+    }
+    
+}
---
+++ EVA2_14_CLASES_ABSTRACTAS2/src/eva2_14_clases_abstractas2/Persona.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_14_clases_abstractas2;
+
+/**
+ *
+ * @author invitado
+ */
+public abstract class Persona {
+    private String nombre;
+    private int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    //los metodos abstractos pueden coexistir co
+    //metodos normales
+  public abstract void imprimirDatos();
+  
+}
