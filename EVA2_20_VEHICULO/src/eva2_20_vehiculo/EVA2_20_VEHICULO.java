fb4add76895c36285adc2804573c120776e74735
omarcar202 <124702322+omarcar202@users.noreply.github.com>
2023-03-24 09:53:30 -0600

    Add files via upload

---
+++ EVA2_20_VEHICULO/src/eva2_20_vehiculo/EVA2_20_VEHICULO.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_20_vehiculo;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_20_VEHICULO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
